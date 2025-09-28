0f4676e640d94fd4d8adf68199847e7b4ba07ece
CodeItQuick <evanontario@hotmail.com>
2025-09-28 08:40:22 -0300

    hook up to mcp server

--- build/index.js
+++ build/index.js
@@ -1,15 +1,25 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
-import { getCommitMessageLogs } from "./git/git-message-logs.js";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mcp_js_1 = require("@modelcontextprotocol/sdk/server/mcp.js");
+const stdio_js_1 = require("@modelcontextprotocol/sdk/server/stdio.js");
+const zod_1 = require("zod");
+const git_message_logs_js_1 = require("./git/git-message-logs.js");
+const git_patch_logs_js_1 = require("./git/git-patch-logs.js");
+const git_directory_logs_js_1 = require("./git/git-directory-logs.js");
 // Create server instance
-const server = new McpServer({
+const server = new mcp_js_1.McpServer({
     name: "git source control mcp server",
     version: "1.0.0",
 });
 server.tool("get-commit-message-logs", "Get git commit message logs for the last number of days", {
-    number_days: z.number().int().min(1).max(6 * 30).describe("integer number of days to retrieve, defaults to seven")
-}, (params) => getCommitMessageLogs(params));
+    number_days: zod_1.z.number().int().min(1).max(6 * 30).describe("integer number of days to retrieve, defaults to seven")
+}, (params) => (0, git_message_logs_js_1.getCommitMessageLogs)(params));
+server.tool("get-commit-patch-logs", "Get git commit patch logs for a particular file", {
+    filename: zod_1.z.string().describe("path and filename to search for the commit patch logs")
+}, (params) => (0, git_patch_logs_js_1.getPatchLogs)(params));
+server.tool("get-directory-filenames", "Get project files within a directory for the project", {
+    directory: zod_1.z.string().describe("path of the files to retrieve. Empty string represents the root directory.")
+}, (params) => (0, git_directory_logs_js_1.getDirectoryLogs)(params));
 // server.tool(
 //     "get-alerts",
 //     "Get weather alerts for a state",
@@ -35,7 +45,7 @@ server.tool("get-commit-message-logs", "Get git commit message logs for the last
 // );
 // Start the server
 async function main() {
-    const transport = new StdioServerTransport();
+    const transport = new stdio_js_1.StdioServerTransport();
     await server.connect(transport);
     console.error("Git Source Control MCP Server running on stdio");
 }
